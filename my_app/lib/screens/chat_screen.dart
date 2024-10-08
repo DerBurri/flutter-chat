0e64bdda25208affcc9ff7243ed792c017b739ec
Maximilian Burr <max@codesphere.com>
2024-10-08 13:50:02 +0000

    create flutter project

---
+++ my_app/lib/screens/chat_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+
