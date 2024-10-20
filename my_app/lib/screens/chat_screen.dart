8485619b73dee990fc8c4bb791510cea31929fe2
Maximilian <burr.maximilian97@gmail.com>
2024-10-20 11:35:35 +0200

    websocket transfer implemented

--- my_app/lib/screens/chat_screen.dart
+++ my_app/lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -7,17 +8,60 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  late WebSocketChannel channel;
   final List<String> messages = [];
   final TextEditingController _messageController = TextEditingController();
 
+  @override
+  void initState(){
+    super.initState();
+    connectToBackend();
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void connectToBackend() {
+    print("Connecting to Backend");
+    channel = WebSocketChannel.connect(
+        Uri.parse('wss://60820-3000.2.codesphere.com/backend/socket'),
+        );
+
+// Listen for incoming messages
+    channel.stream.listen((data) {
+
+      final quotedMessage = data.replaceAll('"', '');
+      // Decode the base64 message
+      final decodedMessage = utf8.decode(base64.decode(quotedMessage));
+      print('Decoded message: $decodedMessage');
+      final message = jsonDecode(decodedMessage);
+      print('Json processed received: ${message['message']}');
+
+
+
+      setState(() {
+        messages.add(message['message']); // Add the decoded message to the list
+      });
+    }, onError: (error) {
+      print('Error: $error');
+    }, onDone: () {
+      print('Connection closed');
+    });
+  }
+  // Send a message to the WebSocket server
   void _sendMessage() {
     final message = _messageController.text;
-    // log 
     print('Message: $message');
     if (message.isNotEmpty) {
+      final messageJson = jsonEncode({'message': message});
+      channel.sink.add(messageJson); // Send message to WebSocket server
       setState(() {
-        messages.add(message);
-        _messageController.clear();
+        messages.add(message); // Add the message to the local list
+        _messageController.clear(); // Clear the input field
       });
     }
   }
