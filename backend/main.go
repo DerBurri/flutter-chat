b326990bce9a8f6b9c1c1d786478723a2407fa9e
Maximilian Burr <max@codesphere.com>
2024-10-24 15:07:01 +0000

    Handled Reconnect in Backend

--- backend/main.go
+++ backend/main.go
@@ -15,7 +15,10 @@ var upgrader = websocket.Upgrader{
 }
 
 var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan [] byte)
+var broadcast = make(chan struct{
+	sender *websocket.Conn
+	message []byte
+})
 
 type Message struct {
 	Name string `json:"username"`
@@ -23,46 +26,62 @@ type Message struct {
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-  ws, err := upgrader.Upgrade(w, r , nil)
+  log.Println("Handling Websocket Connection on /backend/socket")
+  ws, err := upgrader.Upgrade(w, r, nil)
   if err != nil {
-  	log.Fatal(err)
+  	log.Printf("Error upgrading to WebSocket: %v", err)
+  	return
   }
-  defer ws.Close()
+  defer func() {
+  	delete(clients, ws)
+  	ws.Close()
+  }()
 
   clients[ws] = true
 
   for {
-	 //Read message from the client
-	 messageType, p , err := ws.ReadMessage()
-	 if err != nil {
-	 	log.Fatal(err)
-	 }
-
-	 var message Message
-	 err_2 := json.Unmarshal(p, &message)
-	if err_2 != nil {
-		log.Printf("Error in unrmashalling JSON")
-		log.Fatal(err_2)
+	// Read message from the client
+	messageType, p, err := ws.ReadMessage()
+	if err != nil {
+	  log.Printf("Error reading message: %v", err)
+	  break // Exit the loop but don't crash the server
 	}
-	//Sending message to all clients
-	if (messageType == websocket.TextMessage) {
-		broadcast <- p
+
+	var message Message
+	err = json.Unmarshal(p, &message)
+	if err != nil {
+		log.Printf("Error unmarshalling JSON: %v", err)
+		continue // Log the error and continue to the next message
+	}
+
+	// Sending message to all clients
+	if messageType == websocket.TextMessage {
+		broadcast <- struct{
+			sender *websocket.Conn
+			message []byte
+		}{
+		sender: ws,
+		message: p,
 		}
 	}
+  }
 }
 
 func handleMessages() {
 	for {
 		msg := <-broadcast
-		//Send it out to every client that is currently connected
-		for client := range clients{
-		  log.Printf("Sending %s",msg)
-		  msg, _ := json.Marshal(msg)
-		  err := client.WriteMessage(websocket.TextMessage, msg)
+		// Send it out to every client that is currently connected
+		for client := range clients {
+		  if client != msg.sender {
+		  
+		  
+		  log.Printf("Sending: %s", msg.message)
+		  err := client.WriteMessage(websocket.TextMessage, msg.message)
 		  if err != nil {
-		  	log.Printf("error: %v", err)
+			log.Printf("Error sending message: %v", err)
 			client.Close()
-			delete(clients,client)
+			delete(clients, client)
+			}
 		  }
 		}
 	}
@@ -70,10 +89,10 @@ func handleMessages() {
 
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-  	fmt.Fprintf(w, "Chat Backend started and running")})
+  fmt.Fprintf(w, "Chat Backend started and running")})
 
   go handleMessages()
   http.HandleFunc("/backend/socket", handleConnections)
   fmt.Println("WebSocket server started at ws://localhost:3000/ws")
-  fmt.Println(http.ListenAndServe("0.0.0.0:3000", nil))
+  log.Fatal(http.ListenAndServe("0.0.0.0:3000", nil))
 }
