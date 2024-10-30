cdb7a94bc97277f2f7ebc1dee2a48757937e2a17
Maximilian Burr <max@codesphere.com>
2024-10-30 09:24:20 +0000

    removed android sdk

--- configuration.nix
+++ configuration.nix
@@ -7,20 +7,9 @@
   buildToolsVersion ? "30.0.3"
 }:
 
-let
-  # Define Android SDK
-  androidComposition = pkgs.androidenv.composeAndroidPackages {
-    buildToolsVersions = [ buildToolsVersion "28.0.3" ];
-    platformVersions = [ "34" "30" "28" ];
-    abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-	cmdLineToolsVersion = "8.0";
-  };
-  androidSdk = androidComposition.androidsdk;
-in
+
 with pkgs; [
   flutter
-  androidSdk # The customized SDK that we've made above
-  jdk17
   pkg-config
   gtk3
   xorg.libX11
